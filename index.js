78c0d63170d7fbe5791f5ca43793365db0bbb6af
sid-al <sendplusx@gmail.com>
2022-06-11 00:22:43 +0300

    commit tree 05d08b23b18451fce552661ee6214d801a816ed2

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // Code your solution here
+
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+  
+  function findMatching( array, driverLook) {
+      return array.filter(function(driverNames){
+          return driverNames.toLowercase()  === driverLook.toLowercase();
+      })
+  }
+
+
+
+
+//   describe('fuzzyMatch()', function () {
+//     const drivers = [];
+
+//     beforeEach(function () {
+//       drivers.length = 0;
+
+//       drivers.push('Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'bobby');
+//     });
+
+//     it('returns a driver if beginning provided letters match', function () {
+//       expect(fuzzyMatch(drivers, 'Sa')).to.have.members(['Sammy', 'Sarah', 'Sally']);
+//     });
+
+//     it('does not return drivers if only middle or ending letters match', function () {
+//       expect(fuzzyMatch(drivers, 'y')).to.have.members([]);
+//     });
+
+//     it('does not return drivers if only middle or ending letters match', function () {
+//       expect(fuzzyMatch(drivers, 'mm')).to.have.members([]);
+//     });
+//   });
+
+  function fuzzyMatch (array, nameIntials){
+      return array.filter(fuzzName => fuzzName.toLowercase().indexOf(nameIntials.toLowercase())===0) 
+  }
+  
+  function matchName(array, driverName){
+      return array.filter((log) => log.name === driverName);
+  }
+  
